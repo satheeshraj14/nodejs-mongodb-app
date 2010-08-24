d98034a49f7ef2594b05043c90954e6846467c12
satheeshraj14 <satheesh.raj14@gmail.com>
2010-08-24 22:34:14 +0530

    edit html debugging

--- module_mainpage.js
+++ module_mainpage.js
@@ -16,7 +16,7 @@ function add_models(app)
  app.models.mainpage.general.title  = 'Main Page';
  app.models.mainpage.fields.title   = _.extend( _.clone(app.basicfields.normal),{general:{title:'Title'}} );
  app.models.mainpage.fields.text    = _.extend( _.clone(app.basicfields.html),{general:{title:'Text on Main Page'}} );
- app.models.mainpage.fields.footer  = _.extend( _.clone(app.basicfields.html),{general:{title:'Footer of the website'}} );
+ app.models.mainpage.fields.footer  = _.extend( _.clone(app.basicfields.html),{general:{title:'Footer of the website'},list:{use:false}} );
  app.models.mainpage.fields.select = _.cloneextend(app.basicfields.lookup, { general: { title: 'some list' }, edittag: { lookup: { tablename: 'mainpage', linkedfield: '_id', displayfield: 'title' }}});
  app.models.mainpage.links.push ( {name:"othercollection",url:"othercollection.html",func:function (data){ return this.url+'?id='+data.id }} );
 
